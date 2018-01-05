1e0f7f0cdf2690b4ca652ef58a3ec2c77c4304b9
Ben Stone <benjamin.h.stone@gmail.com>
2018-01-05 13:07:18 -0500

    initial commit

---
+++ Unit4Week3Homework-StudentVersion/ZipCodeHelper.swift
@@ -0,0 +1,28 @@
+//
+//  ZipCodeHelper.swift
+//  Unit4Week3Homework-StudentVersion
+//
+//  Created by C4Q  on 1/5/18.
+//  Copyright © 2018 C4Q . All rights reserved.
+//
+
+import Foundation
+
+class ZipCodeHelper {
+    private init() {}
+    static let manager = ZipCodeHelper()
+    func getLocationName(from zipCode: String, completionHandler: @escaping (String) -> Void, errorHandler: @escaping (Error) -> Void) {
+        let geocoder = CLGeocoder()
+        DispatchQueue.global(qos: .userInitiated).async {
+            geocoder.geocodeAddressString(zipCode){(placemarks, error) -> Void in
+                DispatchQueue.main.async {
+                    if let placemark = placemarks?.first, let name = placemark.locality {
+                        completionHandler(name)
+                    } else {
+                        errorHandler(AppError.zipCodeError)
+                    }
+                }
+            }
+        }
+    }
+}
