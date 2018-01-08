4c92ae11f59f51e4659874eba2483602c61e2b70
Melissa He <melissahe@ac.c4q.nyc>
2018-01-08 17:39:38 -0500

    Set up app delegate

--- Unit4Week3Homework-StudentVersion/Views/MainWeatherView.swift
+++ Unit4Week3Homework-StudentVersion/Views/MainWeatherView.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class MainWeatherView: UIView {
     
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
-    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
